565d5ca0b8607bb4ef483ad3ad241682d88d9339
灵玉 <lingyu.csh@taobao.com>
2014-04-18 17:41:01 +0800

    add new branch

--- front-inside/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-filenames=(01-1403)
-
-for i in ${filenames[@]}  
-do
-	echo 'markx ./cn'$i'/markdown.md -t ./template/layout.html > ./cn'$i'/index.html'
-	markx ./cn$i/markdown.md -t ./template/layout.html > ./cn$i/index.html
-done
